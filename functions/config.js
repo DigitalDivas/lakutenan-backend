6ca30d5744ccdf4b23f1034f3f648c64340deb7d
Inaya Rahmanisa <90492681+inayarhmns@users.noreply.github.com>
2023-08-16 14:54:11 +0700

    akhirnya sabi per img ktpan ini

--- functions/config.js
+++ functions/config.js
@@ -3,9 +3,11 @@ const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+  storageBucket: "gs://lakutenan-e6d4f.appspot.com/"
   // Other options if needed
 });
 const db = admin.firestore();
 const User = db.collection("Users");
 const Events = db.collection("Events");
-module.exports = {User, Events};
+const Organizer = db.collection("Organizer");
+module.exports = {User, Events, Organizer};
